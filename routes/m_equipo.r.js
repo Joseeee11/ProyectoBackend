bdbe03aec82fd471459c5c60f44f81f447f79bf6
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-04-23 06:23:38 -0400

    Interfaz Gráfica para ambos mantenimientos

--- routes/m_equipo.r.js
+++ routes/m_equipo.r.js
@@ -9,7 +9,7 @@ const {seccion} = require("../middleware/login.mid")
 router.get('/', seccion, verificador.restringirSolicitante, function(req, res, next) {
     mantenimientoControllers.listar()
     .then ((resultado) => {
-      res.status(200).send(resultado)
+      res.status(200).render('m_equipo', {title: 'TRABAJOS de MANTENIMIENTO para EQUIPOS', resultado: resultado})
     })
     .catch ((err) => {
       res.status(404).send(err)
@@ -20,7 +20,7 @@ router.get('/id:id', seccion,verificador.restringirSolicitante, function(req, re
   const id = req.params.id
   mantenimientoControllers.listarID(id)
   .then((resultado) => {
-    res.status(200).send(resultado);
+    res.status(200).render('m_equipo', {tittle: 'TRABAJOS de MANTENIMIENTO para EQUIPOS', resultado: resultado})
   })
   .catch((err) => {
     res.send(err)
--- routes/m_espacio.r.js
+++ routes/m_espacio.r.js
@@ -9,7 +9,7 @@ const {seccion} = require("../middleware/login.mid")
 router.get('/', seccion, verificador.restringirSolicitante, function(req, res, next) {
     mantenimientoControllers.listar()
     .then ((resultado) => {
-      res.status(200).send(resultado)
+      res.status(200).render('m_espacio', {title: 'TRABAJOS de MANTENIMIENTO para ESPACIOS', resultado: resultado})
     })
     .catch ((err) => {
       res.status(404).send(err)
