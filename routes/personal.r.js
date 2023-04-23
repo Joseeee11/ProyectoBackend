5b915967ec39a70d14b85c20395a3bf08cc59ba6
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-04-22 21:21:23 -0400

    Interfaz personal

--- routes/personal.r.js
+++ routes/personal.r.js
@@ -11,36 +11,39 @@ const bcryptjs = require('bcryptjs');
 router.get('/', seccion, /*verificador.restringirSolicitante,*/ function(req, res, next) {
   personalControllers.listar()
   .then((resultado)=>{
-    res.send(resultado)
+    res.status(200).render('personal', {title: 'PERSONAL TÉCNICO', resultado: resultado });
   })
   .catch((err)=>{
-    res.send(err)
+    res.status(404).render('error')
   })
 });
 //mostrar por cedula
-router.get('/:CI', seccion,verificador.restringirSolicitante, function(req, res, next) {
+router.get('/CI:CI', seccion,verificador.restringirSolicitante, function(req, res, next) {
   const parametro = req.params.CI
   personalControllers.listarCedula(parametro)
   .then((resultado) => {
     res.send(resultado);
   })
   .catch((err) => {
-    res.send(err)
+    res.status(404).render('error')
   })
 });
 
 // post
-router.post('/agregar', seccion, /*verificador.soloAdmin,*/ function(req, res, next) {
+router.get('/agregar', seccion, verificador.restringirSolicitante, function(req, res, next) {
+  res.status(200).render('personalPost', { title: 'Añade un Personal' });
+});
+router.post('/agregar', seccion, verificador.soloAdmin, function(req, res, next) {
   const { usuario_unico, nombre, CI, cargo, especialidad, contrasena} = req.body
   const parametro = { usuario_unico, nombre, CI, cargo, especialidad, contrasena}
   personalControllers.agregar(parametro)
   .then((resultado) => {
     console.log("se agrego correctamente :)")
-    res.send(resultado);
+    res.status(200).redirect('/personal');
   })
   .catch((err) => {
     console.log("error")
-    res.send(err)
+    res.status(404).render('error')
   })
 });
 
